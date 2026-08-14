5c8a6a29bd33de3f59aa83db92b674c66f09823c
Vishakha615 <nikamvishakha954@gmail.com>
2026-08-14 22:13:10 +0530

    Update 1_Login.py

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -10,6 +10,24 @@ st.set_page_config(
 )
 
 
+st.markdown(
+        """
+        <style>
+
+        .stApp {
+            background:
+                linear-gradient(
+                    135deg,
+                    #050816 0%,
+                    #0b1026 50%,
+                    #111936 100%
+                );
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
 st.markdown("""
 <style>
 
