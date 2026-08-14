ee216d80c6fac3331d3bb0c7ecc20650d29f9e70
Vishakha615 <nikamvishakha954@gmail.com>
2026-08-14 22:16:43 +0530

    Update 3_Profile.py

--- pages/3_Profile.py
+++ pages/3_Profile.py
@@ -16,6 +16,29 @@ st.markdown(
         """
         <style>
 
+        /* =========================
+           SIDEBAR
+           ========================= */
+
+        [data-testid="stSidebar"] {
+
+             background:
+                linear-gradient(
+                    180deg,
+                    #0C1430,
+                    #070E29
+                );
+
+            border-right:
+                1px solid
+                rgba(
+                    255,
+                    255,
+                    255,
+                    0.08
+                );
+        }
+        
         .stApp {
             background:
                 linear-gradient(
