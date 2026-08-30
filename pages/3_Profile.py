1ce578c5c8e32e2d7c7638c157c13b607a1d0b07
Vishakha615 <nikamvishakha954@gmail.com>
2026-08-30 19:02:32 +0530

    Update 3_Profile.py

--- pages/3_Profile.py
+++ pages/3_Profile.py
@@ -94,6 +94,16 @@ div.stButton > button:active {
 
 
 # ---------------------------------------------------------
+# SESSION
+# ---------------------------------------------------------
+
+if "logged_in" not in st.session_state:
+
+    st.session_state.logged_in = False
+
+
+
+# ---------------------------------------------------------
 # SIDEBAR
 # ---------------------------------------------------------
 
@@ -158,6 +168,10 @@ with st.sidebar:
     st.page_link(
         "pages/3_Profile.py",
         label="👤 Profile"
+    )
+    st.page_link(
+        "pages/1_Login.py",
+        label="🚪 Logout"
     )
 
 
