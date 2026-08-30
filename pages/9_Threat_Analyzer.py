e87c90401e7117968154e06f47ad6597025638c6
Vishakha615 <nikamvishakha954@gmail.com>
2026-08-30 19:00:56 +0530

    Update 9_Threat_Analyzer.py

--- pages/9_Threat_Analyzer.py
+++ pages/9_Threat_Analyzer.py
@@ -125,7 +125,13 @@ if not st.session_state.get(
     st.stop()
 
 
+# ---------------------------------------------------------
+# SESSION
+# ---------------------------------------------------------
 
+if "logged_in" not in st.session_state:
+
+    st.session_state.logged_in = False
 
 # ---------------------------------------------------------
 # SIDEBAR
@@ -192,6 +198,10 @@ with st.sidebar:
     st.page_link(
         "pages/3_Profile.py",
         label="👤 Profile"
+    )
+    st.page_link(
+        "pages/1_Login.py",
+        label="🚪 Logout"
     )
 
 
